7ba39eb97db54d91a54ce400976f9de387ae1506
newhask777 <newhaskellisp@gmail.com>
2024-12-14 14:27:58 +0300

    init

---
+++ app/Modules/Test/Controllers/TestController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Modules\Test\Controllers;
+
+use App\Modules\Test\Models\Test;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Create of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Modules\Test\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Modules\Test\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Modules\Test\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Test $test)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Modules\Test\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Test $test)
+    {
+        //
+    }
+}
